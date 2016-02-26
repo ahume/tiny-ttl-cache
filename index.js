5211a3761ba093eafe166089abcd88b9f81ba5d5
Andy Hume <ahume@twitter.com>
2016-02-26 09:06:25 +0000

    Avoid clobbering prototype object

--- index.js
+++ index.js
@@ -11,74 +11,71 @@ function TTLCache(maxItems, ttl) {
 	this.ttl = ttl || 1000;
 }
 
-TTLCache.prototype = {
+TTLCache.prototype.put = function (key, value) {
 
-	put: function (key, value) {
+    var node = this.cache[key];
 
-		var node = this.cache[key];
-
-		if (!node) {
-        	// Check the cache size and prune if neccessary.
-            if (this.cacheSize >= this.maxItems) {
-            	var keyToDelete = this.itemList.pop();
-            	delete this.cache[keyToDelete];
-            }
-            node = {};
-            this.cacheSize = this.cacheSize + 1;
+    if (!node) {
+        // Check the cache size and prune if neccessary.
+        if (this.cacheSize >= this.maxItems) {
+            var keyToDelete = this.itemList.pop();
+            delete this.cache[keyToDelete];
         }
+        node = {};
+        this.cacheSize = this.cacheSize + 1;
+    }
 
-        // Set node's new values.
-        node.ttl = Date.now() + this.ttl;
-        node.value = value;
-        node.key = key;
+    // Set node's new values.
+    node.ttl = Date.now() + this.ttl;
+    node.value = value;
+    node.key = key;
 
-        this.itemList.unshift(key);
-		this.cache[key] = {
-			ttl: Date.now() + this.ttl,
-			value: value,
-			key: key
-		};
+    this.itemList.unshift(key);
+    this.cache[key] = {
+        ttl: Date.now() + this.ttl,
+        value: value,
+        key: key
+    };
 
-        // Schedule a flush of expired keys at the next time we know this is required.
-        this.scheduleExpiryFlush(Date.now() + this.ttl);
-	},
+    // Schedule a flush of expired keys at the next time we know this is required.
+    this.scheduleExpiryFlush(Date.now() + this.ttl);
+};
 
-    scheduleExpiryFlush: function (expiryTime) {
-        var now = Date.now();
-        var timeUntilExpiry = expiryTime - now;
+TTLCache.prototype.scheduleExpiryFlush = function (expiryTime) {
+    var now = Date.now();
+    var timeUntilExpiry = expiryTime - now;
 
-        setTimeout(function () {
-            for (var i = this.itemList.length - 1; this.itemList[i]; i--) {
-                var key = this.itemList[i];
-                if (this.cache[key].ttl > now) {
-                    this.itemList.pop();
-                    delete this.cache[key];
-                } else {
-                    // We've hit the first one that hasn't expired. Stop.
-                    break;
-                }
+    setTimeout(function () {
+        for (var i = this.itemList.length - 1; this.itemList[i]; i--) {
+            var key = this.itemList[i];
+            if (this.cache[key].ttl > now) {
+                this.itemList.pop();
+                delete this.cache[key];
+            } else {
+                // We've hit the first one that hasn't expired. Stop.
+                break;
             }
-        }.bind(this), timeUntilExpiry);
-    },
+        }
+    }.bind(this), timeUntilExpiry);
+};
 
-	get: function (key) {
-		var node = this.cache[key];
-		if (!node || node.expiry > Date.now()) {
-			return null;
-		}
+TTLCache.prototype.get = function (key) {
+    var node = this.cache[key];
+    if (!node || node.expiry > Date.now()) {
+        return null;
+    }
 
-		return node.value;
-	},
+    return node.value;
+};
 
-	flush: function () {
-		this._setup();
-	},
+TTLCache.prototype.flush = function () {
+    this._setup();
+};
 
-	_setup: function () {
-		this.cache = {};
-		this.itemList = [];
-		this.cacheSize = 0;
-	}
+TTLCache.prototype._setup = function () {
+    this.cache = {};
+    this.itemList = [];
+    this.cacheSize = 0;
 };
 
 module.exports = TTLCache;
